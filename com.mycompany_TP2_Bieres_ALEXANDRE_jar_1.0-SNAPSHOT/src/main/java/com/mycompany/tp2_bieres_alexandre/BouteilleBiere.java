de19f22c1a9c5b72eda32d394d75f79ebd88a2e9
jason <jason.alexandre@epfedu.fr>
2022-10-28 15:19:52 +0200

    Fin de la partie 1 avec les méthodes décapsuler et toString

--- com.mycompany_TP2_Bieres_ALEXANDRE_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/tp2_bieres_alexandre/BouteilleBiere.java
+++ com.mycompany_TP2_Bieres_ALEXANDRE_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/tp2_bieres_alexandre/BouteilleBiere.java
@@ -9,26 +9,46 @@ package com.mycompany.tp2_bieres_alexandre;
  * @author jason
  */
 public class BouteilleBiere {
-//information de l'étiquette
-String nom;
-double degreAlcool;   
-String brasserie;
-boolean ouverte;
+//on definit les variables de l'étiquette
 
-public void lireEtiquette() {
-System.out.println("Bouteille de " + nom +" (" + degreAlcool +
-" degres) \nBrasserie : " + brasserie ) ;
-}
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
 
-//autre méthode
+    public void lireEtiquette() {
+    // programme pour lire l'étiquette
+        System.out.println("Bouteille de " + nom + " (" + degreAlcool
+                + " degres) \nBrasserie : " + brasserie);
+    }
+
+
     public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+        //on definit les caracaterisques de l'étiquette que l'on complétera pour chaque biere dans le main
         nom = unNom;
         degreAlcool = unDegre;
         brasserie = uneBrasserie;
         ouverte = false;
     }
     
-    
-    
-    
+
+    public boolean Décapsuler() {
+        // programme pour décapsuler les bouteilles
+        if (ouverte == false) {
+            ouverte = true;
+            return ouverte;
+        } else if (ouverte == true) {
+            System.out.println("Bière déjà ouverte");
+            ouverte = false;
+        }
+        return ouverte;
+    }
+        @Override
+        public String toString() {
+            String chaine_a_retourner;
+            chaine_a_retourner = nom + " (" + degreAlcool + " degrés) Ouverte ? ";
+            if (ouverte == true ) chaine_a_retourner += "oui" ;
+            else chaine_a_retourner += "non" ;
+            return chaine_a_retourner ;
+    }
 }
